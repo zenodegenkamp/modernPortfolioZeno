86c3647f38cd3b6255f5a730fc98e56ccb636254
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-10-30 11:12:26 +0100

    fixes small typos

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { slideIn, staggerContainer, textVariant, textVariant2, footerVariants, fadeIn } from '../../utils/motion'
 
 
-export default function Services({darkMode}) {
+export default function Services({ darkMode }) {
 
     return (
         <section className={`h-screen flex flex-col items-center justify-center ${darkMode ? "text-coffee" : "text-verge-black"}`}>
@@ -15,13 +15,14 @@ export default function Services({darkMode}) {
                 className={""}
             >
 
-                <motion.h4 className="text-left sm:px-16 px-6 lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase"  variants={textVariant2}>MY SKILLSET</motion.h4>
+                <motion.h4 className="text-left sm:px-16 px-6 lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase max-w-screen-xl" variants={textVariant2}>MY SKILLSET</motion.h4>
 
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>MASTER, ENTREPRENEURSHIP. 2021.</motion.h4>
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>BACHELOR, INTERNATIONAL BUSINESS ADMINISTRATION. 2020.</motion.h4>
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>MINOR, PROGRAMMING. 2020.</motion.h4>
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>FRONTEND CAREER PATH, SCRIMBA 2023.</motion.h4>
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>UX DESIGN, GROWTH TRIBE 2023.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>master, entrepreneurship. 2021.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>bachelor, international business administration. 2020.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>minor, programming. 2020.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>frontend career path, scrimba 2023.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>ux design, growth tribe 2023.</motion.h4>
+
             </motion.div>
         </section>
 

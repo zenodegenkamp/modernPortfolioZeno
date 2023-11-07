f86e8af63c536c288373f759c5a400e414953b6a
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-11-07 17:39:08 +0100

    small image change and text change

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -18,7 +18,7 @@ export default function Services({ darkMode }) {
                 <motion.h4 className="text-left sm:px-16 px-6 lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase max-w-screen-xl" variants={textVariant2}>MY SKILLSET</motion.h4>
 
                 <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>master, entrepreneurship. 2021.</motion.h4>
-                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>bachelor, international business administration. 2020.</motion.h4>
+                <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>bachelor, business administration. 2020.</motion.h4>
                 <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>minor, programming. 2020.</motion.h4>
                 <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>frontend career path, scrimba 2023.</motion.h4>
                 <motion.h4 className="sm:px-16 px-6 font-bold lg:text-[32px] md:text-[24px] sm:text-[18px] text-[12px] uppercase" variants={textVariant2}>ux design, growth tribe 2023.</motion.h4>

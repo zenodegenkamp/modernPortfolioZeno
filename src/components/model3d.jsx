813f7617888c8266f03349492893c1db8d3c41a3
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-12-11 14:09:30 +0100

    added 3model

---
+++ src/components/model3d.jsx
@@ -0,0 +1,20 @@
+import { motion } from 'framer-motion';
+import { slideIn, staggerContainer, textVariant, textVariant2, footerVariants, fadeIn } from '../../utils/motion'
+import Spline from '@splinetool/react-spline';
+
+
+export default function Model3d(props) {
+
+    console.log(props.darkMode + "hier")
+
+
+
+    return (
+    
+       <section className='h-screen'>
+<iframe src='https://my.spline.design/distortingtypographycopy-829c2939f4dbfe2e75d842cd4998c3eb/' frameborder='0' width='100%' height='100%'></iframe>
+       </section>
+        
+    )
+}
+
